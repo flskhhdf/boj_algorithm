85aaa00a03b389b6afcd34ee33d84a5b5fb6e4f5
flskhhdf <flskhhdf@naver.com>
2023-09-22 13:13:46 +0900

    today solve

--- 7576.cpp
+++ 7576.cpp
@@ -2,43 +2,51 @@
 #include <queue>
 using namespace std;
 
+int map[1001][1001],visit[1001][1001]={0};
+
 int main(){
-	int n,m,map[1001][1001],offset[4][2] = {{-1,0},{0,1},{1,0},{0,-1}},cnt=0,nY,nX;
-	bool visit[1001][1001];
+	int n,m,day = 0, cnt = 0 ;
+	int offset[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}};
 	queue<pair<int,int>> q;
-	queue<pair<int,int>> qq;
 
 	cin >> m >> n;
-
-	for(int i = 0; i<n; i++){
-		for(int j = 0; j<m; j++){
-			cin >> map[i][j];
-			if(map[i][j] == 1) q.push({i,j});
-		}
-	}
 	
-	while(!q.empty()){
-		pair<int,int> tmp = q.front();
-		q.pop();
-		for(int i = 0; i<4; i++){
-			nY = tmp.first + offset[i][0];
-			nX = tmp.second + offset[i][1];
-			if(nY >=0 && nY < n && nX >=0 && nX < m){
-				if(visit[nY][nX] == 0 && map[nY][nX] == 0){
-					map[nY][nX] = 1;
-					visit[nY][nX] = 1;
-				}
-			}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			cin >> map[i][j];
+			if(map[i][j] == 1) q.push({i,j});		
+			else if (map[i][j] == 0) cnt++;
 		}
-
-		for(int i = 0; i<n; i++){
-			for(int j = 0; j<m; j++){
-				cout << map[i][j] << ' ';
-			}
-			cout << '\n';
-		}
-		cout << "=======================\n";
-
 	}
 
+	while(!q.empty()){
+		int qSize = q.size();
+		while(qSize--){
+			pair<int,int> tmp = q.front();
+			visit[tmp.first][tmp.second] = 1;
+			q.pop();
+				
+			for(int k = 0; k<4; k++){
+				int nY = offset[k][0] + tmp.first;
+				int nX = offset[k][1] + tmp.second;
+
+				if(nY >= 0 && nY < n && nX >=0 && nX < m){
+					if(visit[nY][nX] == 0 && map[nY][nX] == 0){
+						map[nY][nX] = 1;
+						q.push({nY,nX});
+					}
+				} 
+			}
+		}
+		day++;
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			if(map[i][j] == 0) {
+				cout << -1;
+				return 0;
+			}
+		}
+	}
+	cout << day-1;
 }
